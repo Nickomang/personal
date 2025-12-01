3a9bb630c41276bf8f50d368ad5ba4ac5e25509f
Nicolas Oman <nickomang@gmail.com>
2025-12-01 11:25:23 -0800

    Initalized Astro project

---
+++ src/content/blog/hello-world.md
@@ -0,0 +1,10 @@
+---
+title: "Hello World"
+description: "First post on the new Astro blog."
+pubDate: "2025-12-01"
+tags: ["meta"]
+---
+
+# Hello World
+
+This is my first post written in Markdown.
---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+// src/content/config.ts
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    description: z.string().optional(),
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  blog,
+};
